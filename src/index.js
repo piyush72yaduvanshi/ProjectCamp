4c17540a1b116abf69e7ee921a97090f2c321459
Piyush Yadav <151051765+piyush72yaduvanshi@users.noreply.github.com>
2025-08-10 18:58:42 +0530

    add index.js in db for make connection to mongoDB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
+
 dotenv.config({
   path: "./.env",
 });
@@ -9,7 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.get("/user", (req, res) => {
   res.send("hello world");
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
