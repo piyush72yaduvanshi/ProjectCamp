3cbf7aa409020e909592a85d25d5996412e2946b
Piyush Yadav <151051765+piyush72yaduvanshi@users.noreply.github.com>
2025-08-11 21:20:12 +0530

    add user model and mail template

---
+++ src/utils/mail.js
@@ -0,0 +1,82 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "ProjectCamp",
+      link: "https://projectcamp.com",
+    },
+  });
+  const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+  const emailHtml = mailGenerator.generate(options.mailgenContent);
+
+  const transport = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
+
+  const mail = {
+    from: "ProjectCampx5P5I@example.com",
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  };
+
+  try {
+    await transport.sendMail(mail);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: `Your account has been created successfully`,
+      action: {
+        instructions: "To verify your account, please click here:",
+        button: {
+          color: "#22BC66",
+          text: "Verify Account",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+import Mailgen from "mailgen";
+
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: `You have requested to reset your password`,
+      action: {
+        instructions: "To reset your password, please click here:",
+        button: {
+          color: "#e11616ff",
+          text: "Reset Password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+export {
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+  sendEmail,
+};
